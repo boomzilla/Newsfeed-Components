e13da2e48b8d18f2e595d1cbcecf8630793c302c
Ian Ruotsala <ian.ruotsala@gmail.com>
2019-04-16 19:44:48 -0700

    updating menu display toggle

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,16 @@
 
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
+  if (menu.display == "block"){
+    menu.display = "none";
+  } else {
+    menu.display = "block";
+  }
 }
 
 // Start Here: Create a reference to the ".menu" class
-const menu;
+const menu = document.getElementsByClassName("menu").item(0);
 // create a reference to the ".menu-button" class
-const menuButton;
+const menuButton = document.getElementsByClassName("menu-button").item(0);
 // Using your menuButton reference, add a click handler that calls toggleMenu
+menuButton.addEventListener("click", toggleMenu);
